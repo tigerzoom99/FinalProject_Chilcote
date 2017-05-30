c948311058843ed64e7a564d97c1c30ae0ae9814
tigerzoom99 <tigerzoom99@gmail.com>
2017-05-30 13:57:07 -0600

    first push

---
+++ src/Converter.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Converter {
+
+	public String changeToHex(int intBase10) {
+		String answer = "";
+		// code for you to complete
+		if (intBase10 < 0 || intBase10 > 255) {
+			JOptionPane.showMessageDialog(null, "Error, range outside of 0-255, please try again");
+			return null;
+		}
+		// basic test
+		if (intBase10 == 0)
+			answer = "00";
+		if (intBase10 == 255)
+			answer = "FF";
+		return answer;
+	}
+
+}
